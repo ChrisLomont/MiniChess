eaeccc8bcfe530b653ca61bf81f24e5c4f8ccb2c
Chris Lomont <clomont@cybernet.com>
2020-08-01 14:18:18 -0400

    starting bitboards

---
+++ Bitboards.cs
@@ -0,0 +1,24 @@
+
+/*
+Sources:
+http://pages.cs.wisc.edu/~psilord/blog/data/chess-pages/index.html
+https://www.chess.com/blog/the_real_greco/representations-of-chess-fen-pgn-and-bitboards
+https://www.chessprogramming.org/Magic_Bitboards
+https://www.chessprogramming.org/Rotated_Bitboards
+https://rhysre.net/2019/01/15/magic-bitboards.html
+https://stackoverflow.com/questions/38728316/not-understanding-how-the-bitboard-technique-works-for-chess-boards/38738728
+https://www.chessprogramming.net/the-beauty-of-bitboards/
+https://chess.stackexchange.com/questions/2831/how-to-represent-chess-state-with-a-bitboard
+*/
+
+// implement bitboards for move generation in chess
+class Bitboards
+{
+    
+    // set bitboard from the given state
+    public void FromState(State state)
+    {
+
+    }
+
+}
